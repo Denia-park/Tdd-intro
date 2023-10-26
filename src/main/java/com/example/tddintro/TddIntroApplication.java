a7f50db06c0d9168726cbdee73497467b1579897
HyunggiPark <phg9587@naver.com>
2023-10-26 22:45:31 +0900

    init

---
+++ src/main/java/com/example/tddintro/TddIntroApplication.java
@@ -0,0 +1,13 @@
+package com.example.tddintro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddIntroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TddIntroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tddintro/TddIntroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tddintro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddIntroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
